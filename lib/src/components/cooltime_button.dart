ffd33d500dda2d6b7011355d9553122f2141e14c
sudarlife <sudarlife@sgma.io>
2021-04-10 11:45:24 +0900

    button cooltime animation 처리

--- lib/src/components/cooltime_button.dart
+++ lib/src/components/cooltime_button.dart
@@ -1,3 +1,4 @@
+import 'package:cooltime_action_button/src/components/progress_animate_container.dart';
 import 'package:cooltime_action_button/src/controller/button_controller.dart';
 import 'package:flutter/material.dart';
 
@@ -10,10 +11,32 @@ class CoolTimeButton extends StatefulWidget {
   _CoolTimeButtonState createState() => _CoolTimeButtonState();
 }
 
-class _CoolTimeButtonState extends State<CoolTimeButton> {
+class _CoolTimeButtonState extends State<CoolTimeButton>
+    with SingleTickerProviderStateMixin {
   bool isActive = true;
+  int delay = 5;
+
+  AnimationController animationController;
+  Animation<double> animation;
+  double _progress = 0;
   @override
   void initState() {
+    animationController =
+        AnimationController(duration: Duration(seconds: delay), vsync: this);
+    animation = Tween(begin: 0.0, end: 1.0).animate(
+      new CurvedAnimation(parent: animationController, curve: Curves.linear),
+    )..addListener(
+        () {
+          setState(
+            () {
+              _progress = animation.value;
+              if (animationController.isCompleted) {
+                _changeState(true);
+              }
+            },
+          );
+        },
+      );
     super.initState();
   }
 
@@ -26,10 +49,8 @@ class _CoolTimeButtonState extends State<CoolTimeButton> {
   void _actionButton() {
     if (isActive) {
       _changeState(false);
+      animationController.forward(from: 0);
       buttonController.action(ButtonType.ACTION1);
-      Future.delayed(Duration(seconds: 1), () {
-        _changeState(true);
-      });
     }
   }
 
@@ -41,29 +62,10 @@ class _CoolTimeButtonState extends State<CoolTimeButton> {
         onTap: _actionButton,
         child: ClipRRect(
             borderRadius: BorderRadius.circular(10),
-            child: Stack(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.grey.shade400,
-                  child: Center(
-                    child: Text(
-                      "BUTTON",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: isActive
-                      ? Colors.transparent
-                      : Colors.black.withOpacity(0.5),
-                ),
-              ],
+            child: ProgressAnimateContainer(
+              progress: _progress,
+              width: 100,
+              height: 100,
             )),
       ),
     );
---
+++ lib/src/components/progress_animate_container.dart
@@ -0,0 +1,59 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ProgressAnimateContainer extends StatelessWidget {
+  final double progress;
+  final double width;
+  final double height;
+  ProgressAnimateContainer({Key key, this.progress, this.width, this.height})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: this.width,
+      height: this.height,
+      child: CustomPaint(
+        painter: ProgressAnimatePainter(progress),
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.grey.withOpacity(0.4),
+          child: Center(
+            child: Text(
+              "BUTTON",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProgressAnimatePainter extends CustomPainter {
+  double progress;
+  ProgressAnimatePainter(this.progress);
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+
+    paint.color = Colors.grey.withOpacity(0.8);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 1;
+
+    Offset center = Offset(size.width / 2, size.height / 2);
+    double arcAngle = 2 * pi * (progress);
+
+    canvas.drawArc(Rect.fromCircle(center: center, radius: 100), -pi / 2,
+        arcAngle, true, paint);
+  }
+
+  @override
+  bool shouldRepaint(ProgressAnimatePainter oldDelegate) {
+    return this.progress != oldDelegate.progress;
+  }
+}
